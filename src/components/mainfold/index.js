0f1a373bbeefc5f88d97a1ed9f306ef1f1a6eb15
Rachit <rachitgup02@gmail.com>
2022-11-23 17:20:50 +0530

    Mainfold

--- src/components/mainfold/index.js
+++ src/components/mainfold/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ITEM_TYPES, ORDER_CARDS_DATA } from '../../common/data'
 import "./mainfold.css"
 const Mainfold = () => {
   return (
@@ -26,7 +27,45 @@ const Mainfold = () => {
       </div>
       <div className="mainfold-main">
         <div className='mainfold-location flex max-width'>
-          Home
+          <div className='mainfold-address-1'>
+              Home
+          </div>
+          <div className='flex absolute-center location-arrow'>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#c9cdd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="5" y1="12" x2="19" y2="12" />
+            <line x1="13" y1="18" x2="19" y2="12" />
+            <line x1="13" y1="6" x2="19" y2="12" />
+            </svg>
+          </div>
+          <div className='mainfold-address-2'>
+            Dehradun
+          </div>
+        </div>
+        <div className='mainfold-info max-width'>
+        <div className='mainfold-city'>Dehradun</div>
+        <div className='mainfold-description'>Why step out when you can get everything delivered home with the tap of a button? Dehradun’s favourite delivery app gets you Food, Grocery, Medicine, Pet Supplies, Fruits & Vegetables, Meat & Fish, Health & Wellness, Gifts and Send Packages from one end of the city to the other. From your local kirana stores to your favourite brands, grocery shopping to your forgotten charger, we are always on the move for you. Why worry about your chores, when you can get it all Dun!</div>
+        </div>
+        <div className='order-cards max-width flex'>
+          {ORDER_CARDS_DATA.map((card)=>{
+            return (
+              <div className='order-card-container'>
+                <img className='cur-po order-card-container' src={card.banner} alt={card.banner}/>
+              </div>
+            )
+          })}
+        </div>
+        <div className='items-cards flex max-width'>
+          {
+            ITEM_TYPES.map((item)=>{
+              return (
+                <div className='items-card cur-po'>
+                  <img className='items-card-banner' src={item.banner} alt={item.title}/>
+                  <div>{item.title}</div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </div>
