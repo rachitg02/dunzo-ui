0d83b5777a758d8e3e124f89fe46ac1e892d2420
Rachit <rachitgup02@gmail.com>
2022-11-23 20:47:19 +0530

    app-download

--- src/components/app-download/index.js
+++ src/components/app-download/index.js
@@ -7,7 +7,14 @@ const AppDownload = () => {
       alt="App Download"
       height={237}
       width={205}/>
-      <div className='flex column'></div>
+      <div className='flex column download-btns'>
+        <div className='download-text'>All this from the convenience of your phone.
+        <br/>Download the Dunzo mobile app.</div>
+        <div>
+          <img alt='Download Icon' src='https://resources.dunzo.com/web-assets/prod/_next/static/images/playstore-ee5b43e66d1583a6066423fb42fb69d8.svg'/>
+          <img alt='Download Icon' src='https://resources.dunzo.com/web-assets/prod/_next/static/images/appstore-078da620a293bb95473ae21624a55872.svg'/>
+        </div>
+      </div>
     </div>
   )
 }
